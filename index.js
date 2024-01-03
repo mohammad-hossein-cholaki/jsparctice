db4ac7a90fe04d1f8f259ec5905aef91bf2308eb
hossein-cholaki <110971150+hossein-cholaki@users.noreply.github.com>
2024-01-03 16:58:25 +0330

    old practice

---
+++ index.js
@@ -0,0 +1,397 @@
+// console.log(2+2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(10/3)
+// console.log(10 % 3)
+// console.log(4 + 3 * 4 / 2)
+// console.log((13 % 5)**2)
+// console.log(200 + 0/0)
+
+// -------------------------------------------------------------------
+
+// let water= 1
+// console.log(water)
+// water++
+// console.log(water)
+// water+=2
+// console.log(water)
+// water-=1
+
+// -------------------------------------------------------------------
+
+// let score = 5
+// score + 4
+// console.log(score)
+
+// -------------------------------------------------------------------
+
+// let wbp = 100
+// wbp/=2
+// wbp +=10
+// console.log(wbp)
+
+// -------------------------------------------------------------------
+
+// let myAge =30;
+// myAge++;
+// console.log(myAge);
+
+// const yourAge= 25;
+// yourAge++;
+// console.log(yourAge);
+
+// -------------------------------------------------------------------
+
+// let isOnline= true
+// console.log(isOnline)
+
+// let isOffline = false
+// console.log(isOffline)
+
+// -------------------------------------------------------------------
+
+
+// string
+
+
+// -------------------------------------------------------------------
+
+
+// let age= '23'
+// age+3;
+// console.log(age)
+// console.log(age+3)
+
+
+
+// const animal= 'Magnificent frigatebird'
+// console.log(animal[7]);
+
+// let lastName = 'yeganeh';
+// let firstName = 'hossein'
+// let space = ' ';
+
+// console.log(firstName + space + lastName)
+
+
+
+// const city = 'Swansea'
+// const country = 'Wales'
+// const comb= city+country
+// console.log(comb)
+
+
+
+// const year = '2000'
+// year + 1;
+// console.log(year)
+// console.log(year+1)
+
+
+
+// let fullName = 'hossein cholaki'
+// console.log(fullName[4])
+// console.log(fullName[30])
+
+
+
+// let fullName = 'hossein cholaki'
+// let nameLentgh = fullName.length
+// console.log(nameLentgh)
+// console.log(fullName.length)
+
+
+
+// let someThing = 'some thing'
+// let result = someThing + 234
+// console.log(result)
+// console.log(result.type)
+// console.log(someThing.type)
+
+
+
+// let firstName= ' Mohammad Hossein  '
+// console.log(firstName.toUpperCase())
+// console.log(firstName.toLowerCase())
+// console.log(firstName.trim())
+// console.log(firstName.trim().length)
+// console.log(firstName.length)
+
+
+
+// string method with argument
+
+
+
+// let firstName= ' Mohammad Hossein  '
+// console.log(firstName.indexOf('H'))
+// console.log(firstName.indexOf('h'))
+// console.log(firstName.indexOf('o'))
+
+
+
+// let pargraph= "you should do your best"
+
+// let result= pargraph.indexOf('do')
+// console.log(result)
+// console.log(pargraph.indexOf('hossein'))
+
+
+
+// let quizeOne = 'skateboard'
+
+// console.log(quizeOne.slice(5).replace('o','e'))
+
+
+
+// let pargraph= "you should do your best"
+
+// console.log(pargraph.slice(5))
+// console.log(pargraph.slice(5,10))
+
+
+
+
+// let names= "hossein mohammad arian ari mohammad"
+// console.log(names)
+// console.log(names.replace('mohammad', 'hossein'))
+// console.log(names.replaceAll('mohammad', 'hossein'))
+
+// console.log('it will finally end. '.repeat(10))
+
+
+// console.log('-------')
+// console.log(Math.random())
+// console.log('-------')
+// console.log(Math.PI)
+// console.log('-------')
+// console.log(Math.round(8.756))
+// console.log('-------')
+// console.log(Math.round(8.239))
+// console.log('-------')
+// console.log(Math.abs(-55))
+// console.log('-------')
+// console.log(Math.pow(2,4))
+// console.log('-------')
+// console.log(Math.floor(2.89))
+// console.log('-------')
+// console.log(Math.ceil(2.89))
+// console.log('-------')
+// console.log(Math.ceil(2.25))
+// console.log('-------')
+
+
+
+// ----------------------------------------------------------------------------
+
+
+// js 1-2
+
+
+
+// let dice1= Math.ceil(Math.random()*6)
+// let dice2= Math.ceil(Math.random()*6)
+
+// console.log(`dice1:${dice1}
+// dice2:${dice2}
+// roll: "you rolled ${dice1} and ${dice2}. they sum to ${dice1 + dice2}"`)
+
+
+
+// console.log(5<2)
+// console.log(10=='10')
+// console.log(10==='10')
+// console.log(3>=2)
+// console.log(-5>=-6)
+
+
+
+// alert('hello')
+
+// let ans= prompt("how old are you?")
+// console.log(ans)
+
+// let ans= 'answer'
+// console.log(typeof(ans))
+
+
+
+// let yourName=prompt('please enter your name:')
+// alert(`Welcome ${yourName}`)
+
+
+
+// let stringNumber= '35'
+// console.log(stringNumber)
+// console.log(stringNumber + 5)
+// console.log(parseInt(stringNumber)+5)
+
+
+
+// let age = parseInt(prompt('please enter your age'))
+
+// if( age < 4){
+//     alert('your so cute')
+// }
+
+
+
+// let number= parseInt(prompt('please enter a number:'))
+
+// if(number % 2 ==0){
+//     alert('your number is even')
+// }else{
+//     alert('your number is odd')
+// }
+
+
+
+// let age = parseInt(prompt('please enter your age'))
+
+// if( age<=80){
+//     alert('you still have time')
+// }else{
+//     alert('your time is over. say good bye to your family')
+// }
+
+
+
+// let age = parseInt( prompt(' please enter your age:'))
+
+// if (age<5){
+//     alert('free')
+// }else if(age>5 && age<12){
+//     alert('5$')
+// }else if( age>12 && age<65){
+//     alert('20$')
+// }else{
+//     alert('free')
+// }
+
+
+
+// let order = prompt('plese give us your order')
+
+// if(order=='red'){
+//     alert('stop')
+// }else if(order== 'yellow'){
+//     alert('take care')
+// }else if( order=='green'){
+//     alert('lets go')
+// }else{
+//     alert('purple')
+// }
+
+
+
+// ------------------------------------------------------------------
+
+
+// console.log(4+3*4/2)
+// console.log((15%5)**2)
+// console.log((15/5)**2)
+
+// ----------------------------------------------------------------
+
+// let age = 5
+// console.log( age)
+// age++
+// console.log(age)
+// age+=2
+// console.log(age)
+// age + 3
+// console.log( age)
+// age = 6
+// console.log(age)
+
+// --------------------------------------------------------------
+
+// let score = 4
+
+// score + 6
+
+// console.log(score)
+
+// let wbp = 100
+// wbp /= 2
+// wbp += 10
+// console.log(wbp)
+
+// const myAge= 29
+// myAge++
+// console.log(myAge)
+
+// let isOnline = true
+// console.log(isOnline)
+
+// let firstName = 'Arian'
+// let lastName = 'Cholaki '
+
+// let email = "'arian' @gmail.com"
+// console.log(email)
+// let myName= `${firstName} ${lastName}`
+
+// console.log(myName)
+// console.log(myName.length)
+// console.log(myName.trim().length)
+// console.log(myName[0])
+// console.log(myName[1])
+// console.log(myName[19])
+// console.log(firstName+' '+lastName)
+// console.log(myName.toLowerCase())
+// console.log(myName.toUpperCase())
+
+// const animal='Magnificent'
+// console.log(animal[7])
+// const city = 'Swansea'
+// const country= 'Wales'
+// const comb= city + country
+// console.log(comb)
+
+// -----------------------------------------------------------
+
+// let year='2000'
+// console.log(year+1)
+// console.log(myName.indexOf('i'))
+// console.log(myName.indexOf('Cholaki'))
+// console.log(myName.indexOf('c'))
+// console.log(myName.indexOf('C'))
+// console.log(myName.indexOf('an'))
+// let felan=myName.slice(6)
+// console.log(felan)
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+
+
+// console.log(str.slice(31))
+// console.log(str.slice(4,19))
+// console.log(str.slice(-4))
+// console.log(str.slice(-1))
+// console.log(str.slice(-9, -5))
+
+// let kalame = 'skateboard'
+// console.log(kalame.slice(5).replace('o','e'))
+
+// const paragraph = "I think Ruth's dog is cuter than your dog!";
+// console.log(paragraph.replace("Ruth's",'my'))
+
+// const regex = /Dog/i;
+// console.log(paragraph.replace(regex, 'ferret'));
+
+// console.log(paragraph.replace((paragraph[0],paragraph[2])))
+
+// let jomle = 'hossein amir arian ari mohammad hossein'
+// console.log(jomle.replaceAll('hossein', 'ari'))
+
+// console.log(Math.random())
+// console.log(Math.PI)
+// console.log(Math.round(3.6))
+// console.log(Math.round(3.2))
+// console.log(Math.round(3.5))
+// console.log(Math.round(3.4))
+// console.log(Math.abs(-654))
+// console.log(Math.pow(2,3))
+// console.log(Math.floor(2.99))
+
+// ---------------------------------------------------------------
+
